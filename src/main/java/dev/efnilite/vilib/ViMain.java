56142bb21ebe7e822eb97fe94e06e2df04cf65a3
Efnilite <re.verstappen@gmail.com>
2022-04-23 01:14:50 +0200

    Add plugin Elevator

---
+++ src/main/java/dev/efnilite/vilib/ViMain.java
@@ -0,0 +1,14 @@
+package dev.efnilite.vilib;
+
+public class ViMain extends ViPlugin {
+
+    @Override
+    public void enable() {
+        // todo update checker
+    }
+
+    @Override
+    public void disable() {
+
+    }
+}
