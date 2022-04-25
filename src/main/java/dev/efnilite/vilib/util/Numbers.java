c62e2b055f0a91369a21d77dee546f1a75d9ad41
Efnilite <re.verstappen@gmail.com>
2022-04-25 17:55:11 +0200

    Improve number utils

--- src/main/java/dev/efnilite/vilib/util/Numbers.java
+++ src/main/java/dev/efnilite/vilib/util/Numbers.java
@@ -2,11 +2,27 @@ package dev.efnilite.vilib.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Number utils
  */
 public class Numbers {
+
+    /**
+     * Returns a random real number between two bounds.
+     *
+     * @param   lower
+     *          The lower bound (inclusive)
+     *
+     * @param   upper
+     *          The upper bound (exclusive)
+     *
+     * @return a random real number between the two bounds
+     */
+    public static int random(int lower, int upper) {
+        return ThreadLocalRandom.current().nextInt(lower, upper);
+    }
 
     /**
      * Gets all ints from 0 (inclusive) to n (inclusive)
@@ -42,6 +58,14 @@ public class Numbers {
         return min;
     }
 
+    /**
+     * Gets the max int from an array of ints
+     *
+     * @param   ints
+     *          All ints
+     *
+     * @return the int with the biggest value
+     */
     public static int max(int... ints) {
         int max = Integer.MIN_VALUE;
 
@@ -52,6 +76,17 @@ public class Numbers {
         return max;
     }
 
+    /**
+     * Returns all real numbers between two bounds
+     *
+     * @param   from
+     *          The lower bound (inclusive)
+     *
+     * @param   to
+     *          The upper bound (inclusive)
+     *
+     * @return a list of all real numbers between the two bounds
+     */
     public static List<Integer> getFromTo(int from, int to) {
         List<Integer> result = new ArrayList<>();
         for (int i = min(from, to); i <= max(from, to); i++) {
